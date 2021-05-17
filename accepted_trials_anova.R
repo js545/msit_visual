6d077f0cd9dfee3c8accb70e6f747010d6535353
Son <j.son5457@gmail.com>
2021-05-17 15:38:16 -0500

    ANOVA across conditions, still significant difference in trial count after removing participants with all_correct < 2.5sdev from mean

--- accepted_trials_anova.R
+++ accepted_trials_anova.R
@@ -14,10 +14,37 @@ for (i in 2:length(filenames_list)) {
   
 }
 
-mean_all_correct = mean(df_concat$All_Correct_Accepted_Trials)
-mean_flanker_correct = mean(df_concat$Identity_Accepted_Trials)
-mean_simon_correct = mean(df_concat$Spatial_Accepted_Trials)
-mean_control_correct = mean(df_concat$Control_Accepted_Trials)
-mean_msit_correct = mean(df_concat$Multi_Source_Accepted_Trials)
+mean_all = mean(df_concat$All_Correct_Accepted_Trials)
+sd_all = sd(df_concat$All_Correct_Accepted_Trials)
+mean_flanker = mean(df_concat$Identity_Accepted_Trials)
+sd_flanker = sd(df_concat$Identity_Accepted_Trials)
+mean_simon = mean(df_concat$Spatial_Accepted_Trials)
+sd_simon = sd(df_concat$Spatial_Accepted_Trials)
+mean_control = mean(df_concat$Control_Accepted_Trials)
+sd_control = sd(df_concat$Control_Accepted_Trials)
+mean_msit = mean(df_concat$Multi_Source_Accepted_Trials)
+sd_msit = sd(df_concat$Multi_Source_Accepted_Trials)
+
+threshold = mean_all - 2.5*sd_all
+df_concat = subset(df_concat, df_concat$All_Correct_Accepted_Trials > threshold)
 
 # Reorganize data
+
+num_samples = dim(df_concat)[1]
+
+num_correct = c(df_concat$Control_Accepted_Trials, df_concat$Spatial_Accepted_Trials,
+                df_concat$Identity_Accepted_Trials, df_concat$Multi_Source_Accepted_Trials)
+condition_label = c(rep(c('Control'), num_samples), rep(c('Spatial'), num_samples),
+                    rep(c('Identity'), num_samples), rep(c('MSIT'), num_samples))
+anova_df = as.data.frame(cbind(num_correct, condition_label))
+
+anova_df[,1] = as.integer(as.character(anova_df[,1]))
+
+# Run ANOVA
+
+res.aov = aov(num_correct ~ condition_label, data=anova_df)
+summary(res.aov)
+
+
+
+
